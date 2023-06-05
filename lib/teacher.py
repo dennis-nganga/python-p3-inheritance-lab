aba351279992bb63959cafdeee7e91e9f7be7927
dennis-nganga <denniskimani416@gmail.com>
2023-06-05 23:27:26 +0300

    first commit

--- lib/teacher.py
+++ lib/teacher.py
@@ -1,10 +1,24 @@
 #!/usr/bin/env python
-
 from user import User
 
 import random
 
 class Teacher(User):
+    def __init__(self, first_name, last_name, knowledge):
+        super().__init__(first_name, last_name)
+        self.knowledge = knowledge
 
     def teach(self):
-        pass
+        for topic in self.knowledge:
+            print("teaching:", topic)
+
+my_teacher= Teacher("my","Teacher",[  "str is a data type in Python",
+        "programming is hard, but it's worth it",
+        "JavaScript async web request",
+        "Python function call definition",
+        "object-oriented teacher instance",
+        "programming computers hacking learning terminal",
+        "pipenv install pipenv shell",
+        "pytest -x flag to fail fast",])
+    
+my_teacher.teach()
