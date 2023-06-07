69212ad6919693b6ce95e7e3cee899dbbf84f8d4
dennis-nganga <denniskimani416@gmail.com>
2023-06-07 05:55:42 +0300

    complete

--- lib/teacher.py
+++ lib/teacher.py
@@ -4,21 +4,19 @@ from user import User
 import random
 
 class Teacher(User):
-    def __init__(self, first_name, last_name, knowledge):
+
+    def __init__(self, first_name, last_name):
         super().__init__(first_name, last_name)
-        self.knowledge = knowledge
+        self.knowledge = [
+            "str is a data type in Python",
+            "programming is hard, but it's worth it",
+            "JavaScript async web request",
+            "Python function call definition",
+            "object-oriented teacher instance",
+            "programming computers hacking learning terminal",
+            "pipenv install pipenv shell",
+            "pytest -x flag to fail fast",
+        ]
 
     def teach(self):
-        for topic in self.knowledge:
-            print("teaching:", topic)
-
-my_teacher= Teacher("my","Teacher",[  "str is a data type in Python",
-        "programming is hard, but it's worth it",
-        "JavaScript async web request",
-        "Python function call definition",
-        "object-oriented teacher instance",
-        "programming computers hacking learning terminal",
-        "pipenv install pipenv shell",
-        "pytest -x flag to fail fast",])
-    
-my_teacher.teach()
+        return self.knowledge[random.randint(0, len(self.knowledge) - 1)]
